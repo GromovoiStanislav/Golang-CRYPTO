17a8ea42bf351a712f1fa622a167a76af8a514a4
Gromovoi Stanislav <gromila12@rambler.ru>
2023-11-08 17:24:29 +0600

    asymmetric

--- asymmetric/example2.go
+++ asymmetric/example2.go
@@ -3,7 +3,6 @@ package main
 import (
 	"crypto/rand"
 	"crypto/rsa"
-	"crypto/sha256"
 	"encoding/base64"
 	"fmt"
 )
@@ -20,24 +19,25 @@ func main() {
     message := "Hello, world!"
 
     // Шифруем сообщение публичным ключом
-    encrypted, err := rsa.EncryptOAEP(sha256.New(), rand.Reader, &privateKey.PublicKey, []byte(message),[]byte("my_solt"))
-    if err != nil {
-        fmt.Println("Ошибка при шифровании сообщения")
-        panic(err)
-    }
+    encrypted, err := rsa.EncryptPKCS1v15(rand.Reader,  &privateKey.PublicKey, []byte(message))
+	if err != nil {
+		fmt.Println("Ошибка при шифровании сообщения")
+		panic(err)
+	}
+    
 	// Выводим шифрованное сообщение
-    //fmt.Println(encrypted) // байты
+    //fmt.Println(encrypted) //байты
 
     // Преобразование зашифрованного сообщения в Base64
     encryptedBase64 := base64.StdEncoding.EncodeToString(encrypted)
     fmt.Println(encryptedBase64)
 
+
     // Декодирование сообщения из Base64 в байты
 	encryptedMessage, err := base64.StdEncoding.DecodeString(string(encryptedBase64))
 
-
     // Дешифруем сообщение закрытым ключом
-    decrypted, err := rsa.DecryptOAEP(sha256.New(), rand.Reader, privateKey, encryptedMessage,[]byte("my_solt"))
+    decrypted, err := rsa.DecryptPKCS1v15(rand.Reader, privateKey, encryptedMessage)
     if err != nil {
         fmt.Println("Ошибка при дешифровании сообщения")
         panic(err)
